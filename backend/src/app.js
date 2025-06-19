aaa33d4afe66270e90f116c9f34a69a31c2ffcf4
MacBook <macbook@192.168.1.11>
2025-06-18 21:09:28 -0400

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,10 @@ import db from "./config/db.js";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://parking-app-frontend.pages.dev',
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -25,11 +28,11 @@ app.use("/api/reports", reportRoutes);
 db.authenticate()
   .then(async () => {
     console.log("Conectado a PostgreSQL")
-    //await initializeParkings(); 
+    await initializeParkings(); 
   })
   .catch((err) => console.error("Error al conectar la BD:", err));
 
-db.sync({ alter:false}) 
+db.sync({ alter: true}) 
   .then(() => console.log("Base de datos conectada"))
   .catch((err) => console.log("Error al conectar la BD", err));
 
