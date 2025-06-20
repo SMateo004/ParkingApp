b4d80b6408a539fb013a2f868158f7386a963d81
MacBook <macbook@192.168.1.11>
2025-06-19 23:10:37 -0400

    more fixes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
 import app from "./src/app.js";
+import db from "./src/config/db.js";
 import "./src/models/User.js";
 import "./src/models/Parking.js";
 import "./src/models/Reservation.js";
 import "./src/models/Vehicle.js";
 import "./src/utils/associations.js";
+import { initializeParkings } from "./src/services/parkingService.js";
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await db.authenticate();
+    console.log("✅ Conectado a PostgreSQL");
+
+    await db.sync({ alter: true }); // o { alter: true } en producción
+    console.log("✅ Base de datos sincronizada");
+
+    await initializeParkings(); // Ya no hace syncs internos
+    console.log("✅ Datos iniciales insertados");
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("❌ Error al iniciar la aplicación:", error);
+  }
+};
+
+startServer();
--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,6 @@ import vehicleRoutes from "./routes/vehicleRoutes.js";
 import parkingRoutes from "./routes/parkingRoutes.js";
 import reservationRoutes from "./routes/reservationRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
-import { initializeParkings } from "./services/parkingService.js";
-
-import db from "./config/db.js";
 
 dotenv.config();
 const app = express();
@@ -24,16 +21,5 @@ app.use("/api/vehicles", vehicleRoutes)
 app.use("/api/parkings", parkingRoutes);
 app.use("/api/reservations", reservationRoutes);
 app.use("/api/reports", reportRoutes);
-
-db.authenticate()
-  .then(async () => {
-    console.log("Conectado a PostgreSQL")
-    await initializeParkings(); 
-  })
-  .catch((err) => console.error("Error al conectar la BD:", err));
-
-db.sync({ alter: true}) 
-  .then(() => console.log("Base de datos conectada"))
-  .catch((err) => console.log("Error al conectar la BD", err));
 
 export default app;
